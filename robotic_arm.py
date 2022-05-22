53933090f1db13806b6760bab1ad66f4c13894c2
Vlad Andrei VASILE <vlad.vasile1907@stud.acs.upb.ro>
2022-05-22 22:52:59 +0300

    refactoring

--- robotic_arm.py
+++ robotic_arm.py
@@ -34,9 +34,9 @@ class ServoCoords:
         return ServoCoords(self.s1 + other.s1, self.s2 + other.s2, self.s3 + other.s3)
 
     def __truediv__(self, other):
-        return ServoCoords(self.s1 / other,
-                           self.s2 / other,
-                           self.s3 / other)
+        return ServoCoords(int(self.s1 / other),
+                           int(self.s2 / other),
+                           int(self.s3 / other))
 
     def clamp_all(self):
         self.s1 = clamp(self.s1, 0, 180)
@@ -47,7 +47,6 @@ class ServoCoords:
 def robot_arm():
     # Webcam record
     video_capture = cv2.VideoCapture(0)
-
     # Video Playback Resolution
     image_dimensions = Point2D(1280, 720)
     video_capture.set(3, image_dimensions.x)
@@ -56,71 +55,52 @@ def robot_arm():
     # Hand rectangle
     hand_rect_pos = Point2D(448, 144)
     hand_rect_dim = Point2D(384, 432)
-
-    init_iter = 0
-
-    # Servo Coordinates
-    servo_coords = ServoCoords()
-
     # Hand Detection
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5)
-
     # Hand Drawer
     mp_draw = mp.solutions.drawing_utils
-
-    setup = False
 
     # Connect to Arduino
     arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
     # Wait for Arduino to be ready
-    print(arduino.readline())
+    arduino.readline()
 
-    iterations = 0
+    # Setup: Wait for stable input (hand in rectangle)
+    setup = False
+    init_iter = 0
 
-    servo_sum = ServoCoords(0, 0, 0)
+    # Servo Coordinates
+    servo_coords = ServoCoords()
+    # Number of iterations before sending data to arduino
     max_iter = 8
+    iterations = 0
+    # Servo sum of max_iter servo coordinates
+    servo_sum = ServoCoords(0, 0, 0)
 
     while True:
         success, image = video_capture.read()
-        distance = 0
-        x1 = 0
-        y1 = 0
-        x2 = 0
-        y2 = 0
-
+        x1 = y1 = x2 = y2 = distance = 0
         landmarks = []
         draw_hand_landmarks(hands, image, image_dimensions, landmarks, mp_draw, mp_hands)
 
-        # If hand was not detected
+        # If hand was not detected, reset
         if not landmarks:
             setup = False
             init_iter = 0
+        # Get hand coordinates and distance
         else:
-            x1, y1 = landmarks[5][1], landmarks[5][2]
-            x2, y2 = landmarks[17][1], landmarks[17][2]
-            if x1 > x2:
-                x1, x2 = x2, x1
-            if y1 > y2:
-                y1, y2 = y2, y1
+            x1, x2, y1, y2 = get_coordinates(landmarks)
+            # If hand gets out of screen, reset
             if x1 <= 0 or x2 >= image_dimensions.x or y1 <= 0 or y2 >= image_dimensions.y:
                 setup = False
                 init_iter = 0
             distance = math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
 
+        # If setup was not performed, check if hand is situated inside the
+        # rectangle for 10 consecutive iterations
         if not setup:
-            cv2.rectangle(image,
-                          (hand_rect_pos.x, hand_rect_pos.y),
-                          (hand_rect_pos.x + hand_rect_dim.x, hand_rect_pos.y + hand_rect_dim.y),
-                          (255, 0, 0),
-                          2)
-            cv2.putText(image,
-                        f"Place your hand in the rectangle",
-                        (hand_rect_pos.x - 160, hand_rect_pos.y - 30),
-                        cv2.FONT_HERSHEY_TRIPLEX,
-                        1.25,
-                        (200, 0, 50),
-                        3)
+            image_print(hand_rect_dim, hand_rect_pos, image)
             if point_rectangle(x1, y1, hand_rect_pos.x, hand_rect_pos.y, hand_rect_dim.x,
                                hand_rect_dim.y) and point_rectangle(
                 x2, y2, hand_rect_pos.x, hand_rect_pos.y, hand_rect_dim.x, hand_rect_dim.y):
@@ -128,26 +108,48 @@ def robot_arm():
                 init_iter += 1
                 if init_iter == 10:
                     setup = True
-
+        # Transform hand coordinates to servo coordinates
         else:
             servo_coords = get_servo_coords(distance, image_dimensions, x1, x2, y1, y2)
-            # print(f"servo_coords.s1: {servo_coords.s1}, servo_coords.s2: {servo_coords.s2}, servo_coords.s3: {servo_coords.s3}")
 
         servo_sum = servo_sum + servo_coords
-        # print(f"servo_sum.s1: {servo_sum.s1}, servo_sum.s2: {servo_sum.s2}, servo_sum.s3: {servo_sum.s3}")
-
         iterations = iterations + 1
+        # Transmit data to arduino serial every max_iter iterations
         if iterations == max_iter:
             servo_sum /= max_iter
-
-            print(bytes(str(servo_coords.s1) + " " + str(servo_coords.s2) + " " + str(servo_coords.s3), 'utf-8'))
-            arduino.write(bytes(str(servo_coords.s1) + " " + str(servo_coords.s2) + " " + str(servo_coords.s3), 'utf-8'))
+            arduino_message = str(servo_sum.s1) + " " + str(servo_sum.s2) + " " + str(servo_sum.s3)
+            arduino.write(bytes(arduino_message, 'utf-8'))
             servo_sum = ServoCoords(0, 0, 0)
-        iterations = iterations % max_iter
+            iterations = 0
 
         cv2.imshow('Image', image)
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
+
+
+def image_print(hand_rect_dim, hand_rect_pos, image):
+    cv2.rectangle(image,
+                  (hand_rect_pos.x, hand_rect_pos.y),
+                  (hand_rect_pos.x + hand_rect_dim.x, hand_rect_pos.y + hand_rect_dim.y),
+                  (255, 0, 0),
+                  2)
+    cv2.putText(image,
+                f"Place your hand in the rectangle",
+                (hand_rect_pos.x - 160, hand_rect_pos.y - 30),
+                cv2.FONT_HERSHEY_TRIPLEX,
+                1.25,
+                (200, 0, 50),
+                3)
+
+
+def get_coordinates(landmarks):
+    x1, y1 = landmarks[5][1], landmarks[5][2]
+    x2, y2 = landmarks[17][1], landmarks[17][2]
+    if x1 > x2:
+        x1, x2 = x2, x1
+    if y1 > y2:
+        y1, y2 = y2, y1
+    return x1, x2, y1, y2
 
 
 def get_servo_coords(distance, image_dimensions, x1, x2, y1, y2):
